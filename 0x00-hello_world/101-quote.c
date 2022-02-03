bb5d1838c2af7b9599d07d62056a3c606f408aee
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-03 23:42:16 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 11);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 
 	return 1;
 }
