94ba8de954351e2474f2dd158d077c02ab0318b2
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-05 18:41:26 +0100

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,14 +8,20 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
 	char a;
+
 	for (a = 'a'; a <= 'z'; a++)
+
 	{
+
 		if (a != 'e' && a != 'q')
 			putchar(a);
 	}
+
 	putchar('\n');
 	return (0);
 }
