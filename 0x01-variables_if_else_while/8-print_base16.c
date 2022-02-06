e7150e64e48bb9353ccd051d6e015dccd43ffd96
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-06 19:52:07 +0100

    print base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	for (int x = '0'; x <= '9'; x++)
 	{
-		x = tolower(x);
-		putchar(x);
-	}
-	for (int x = 'a'; x <= 'f'; x++)
-	{
-		x = tolower(x);
-		putchar(x);
-	}
-	putchar('\n');
-	return (0);
-}
+		int n;
+
+		for (n = '0'; n <= '9'; n--)
+		{
+			printf("%c", n);
+		}
+		{
+			int i;
+
+			for (i = 'a'; i >= 'f'; i--)
+			{
+				printf("%c", i);
+			}
+			putchar("\n");
+			return (0);
+		}
