2d58c46bc7d6e1cc28cf412279b1d9430eddce33
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-07 17:23:48 +0100

    print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = '0'; a < '9'; a++)
+
+	{
+
+		for (b = '0'; b <= '9'; b++)
+
+		{
+			for (c = '0'; c <= '8'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+				}
+
+				if (a < b)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (a == '9' && b == '9' && c == '8' && d == '9')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
