ce9e556c736ba9ab5b6f3ad30f1e8404c8821a16
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-07 16:27:02 +0100

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '7'; a++)
+
+	{
+
+		for (b = '1'; b <= '8'; b++)
+		{
+			for (c = '2'; c <= '9'; c++)
+
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a == '7' && b == '8' && c == '9')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
