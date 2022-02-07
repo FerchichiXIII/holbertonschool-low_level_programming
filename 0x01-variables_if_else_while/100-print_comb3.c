1ed308700be5a90c82700ee82bfe1e80898a4c7e
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-07 15:36:43 +0100

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,20 +10,24 @@ int main(void)
 
 	for (a = '0'; a < '9'; a++)
 
-		;
-	for (b = '1'; b <= '9'; b++)
-
 	{
-		if (a < b)
+
+		for (b = '1'; b <= '9'; b++)
+
 		{
-			putchar(a);
-			putchar(b);
-			if (a == '8' && b == '9')
-				continue;
-			putchar(',');
-			putchar(' ');
+			if (a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a == '8' && b == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+
 		}
+
 	}
-	putchar('\n');
-	return ('0');
-}
+		putchar('\n');
+		return ('0');
+	}
