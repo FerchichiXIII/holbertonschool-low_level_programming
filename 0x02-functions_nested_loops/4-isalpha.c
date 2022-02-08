21aedee98ca5975e7543754e2ab5f75239ed0b76
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-08 23:42:16 +0100

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- * _islower -  checks for alphabetic character
- *
+ * _isalpha - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 'A' && c <= 'Z') ||
