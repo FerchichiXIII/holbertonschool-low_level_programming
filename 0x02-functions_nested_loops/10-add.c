edeb2317b79427a98e7d88f694eab7f9f8b05b8d
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-09 19:59:06 +0100

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - Adds two integers and returns the result.
- *
- *
+ * @a: The character to print
+ * @b: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
