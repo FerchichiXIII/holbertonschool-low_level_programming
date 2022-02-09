78dbec82eda4a1b38f9311752fd76bf1a69f3bdb
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-09 18:28:30 +0100

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,12 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: The character to print
- * @y: The character to print
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int print_last_digit(int)
+int print_last_digit(int n)
 
 {
 
