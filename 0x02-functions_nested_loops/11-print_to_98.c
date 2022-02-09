5212c36226bc3ed79ff51fc73c635652a45d00ee
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-09 22:22:08 +0100

    test

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_to_98(int n)
+{
+
+	if (n = 0)
+	{
+		while (n <= 98)
+		{
+			continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+	if (n = 111)
+	{
+		while (n >= 98)
+		{
+			continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+		if (n = -10)
+		{
+			while (n <= 98)
+			{
+				continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
