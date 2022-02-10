155fc7dc15b4558458604f1a0ac0b1da93c4d1c9
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-10 23:58:02 +0100

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * mul -  multiplies two integers.
- * @c: The character to print
- *
+ * mul - multiplies two integers
+ * @a: The character to print
+ * @b: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int mul(int a, int b)
 {
 	return (a * b);
