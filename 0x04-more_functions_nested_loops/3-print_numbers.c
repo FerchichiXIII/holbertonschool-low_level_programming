3e102718a008e22c9e6ea38f767b2b4ccffbd34a
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-11 09:49:58 +0100

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * print_numbers -  prints the numbers, from 0 to 9,
- * @i: The character to print
+ *
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void print_numbers(void)
+
 {
 	int i = 0;
 
