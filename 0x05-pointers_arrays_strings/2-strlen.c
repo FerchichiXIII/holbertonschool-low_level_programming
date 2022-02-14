c0a4a076a31d17569bd87f4e7b7dea5e268fb4ed
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-14 21:51:48 +0100

    strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 /**
  *_strlen - returns the length of a string.
  *
- *@s: strlen to n
+ *@s: strlen to n.
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
