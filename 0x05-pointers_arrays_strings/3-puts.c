a99157bced48b8739d73d80d0259c689e05a5c20
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-14 22:51:32 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_puts - returns the length of a string.
+ *
+ *@str: prints a string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
