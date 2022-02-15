b2d8390b053b724029d7f9c833e4fe5e124572ca
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-15 15:36:43 +0100

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ *@str: character.
+ */
+void puts_half(char *str)
+{
+	int i, n = 0, j;
+	char *l;
+
+	for (l = str; *l; l++)
+		n += 1;
+	if (n % 2 == 0)
+	{
+		j = n / 2;
+		for (i = j; i < n; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		j = (n - 1) / 2;
+		for (i = j + 1; i < n; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+
+	_putchar(10);
+}
