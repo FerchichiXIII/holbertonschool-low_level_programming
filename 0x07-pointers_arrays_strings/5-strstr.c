4d51d1e20bf2aa332d7de10d3f6492dfdfe71f00
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-22 00:05:00 +0100

    passworld

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *_strpbrk - gets the length of a prefix substring.
+ * @s: char
+ * @accept: char
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
