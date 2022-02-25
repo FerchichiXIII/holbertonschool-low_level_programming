738aee3a3884525be814b1ae2ce8c52df35e9b86
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-25 22:49:15 +0100

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  prints its name, followed by a new line.
+ *@argc: int
+ *@argv: char
+ * Return: always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int i, sum;
+
+	if (argc == 1 || argc == 2)
+
+	{
+
+		printf("ERROR\n");
+
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum = atoi(argv[1]) * atoi(argv[2]);
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
