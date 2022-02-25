ce9eec5aea9056a5aa6041e7a5c6fb543f3f9e78
FerchichiXIII <anasferchichi22@gmail.com>
2022-02-25 12:41:58 +0100

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  prints its name, followed by a new line.
+ *@argc: int
+ *@argv: char
+ * Return: always 0.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	puts(argv[0]);
+	return (0);
+}
