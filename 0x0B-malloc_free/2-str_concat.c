11db68ab5223f0915ebc57399754837ae61cb294
FerchichiXIII <anasferchichi22@gmail.com>
2022-03-01 22:30:46 +0100

    str

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * str_concat - duplicate string in new memory addr
+ * @s1: char
+ * @s2: char
+ * Return: trn
+ **/
+char *str_concat(char *s1, char *s2)
+{
+	char *p, *q, *rtn;
+
+	rtn = q = malloc(strlen(s1) + strlen(s2) + 1);
+
+	for (p = s1; (*q = *p) != '\0'; ++p, ++q)
+	{
+	}
+	for (p = s2; (*q = *p) != '\0'; ++p, ++q)
+	{
+	}
+	return (rtn);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+char *str_concat(char *s1, char *s2);
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
