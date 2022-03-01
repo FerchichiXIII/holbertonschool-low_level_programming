7bbd79590db747de39356e79280bbb1d430614be
FerchichiXIII <anasferchichi22@gmail.com>
2022-03-01 22:20:34 +0100

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+
+#include "main.h"
+
+/**
+ * _strdup - duplicate string in new memory addr
+ * @str: string
+ *
+ * Return: new string addr
+ **/
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int l, i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+
+		l = strlen(str);
+		s = malloc(l + 1);
+		if (s == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < l; i++)
+		{
+			s[i] = str[i];
+		}
+	}
+
+	return (s);
+}
