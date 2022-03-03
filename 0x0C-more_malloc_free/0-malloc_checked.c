05dfb7138271fffba948b59bd3170954f2c634db
FerchichiXIII <anasferchichi22@gmail.com>
2022-03-03 20:31:28 +0100

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * malloc_checked -	allocates memory using
+ *@b:int
+ *Return: prt
+ *
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = (int *)malloc(b * sizeof(int));
+	if (ptr == NULL)
+	{
+
+		exit(98);
+		return (NULL);
+		free (ptr);
+	}
+	else
+		return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+void *malloc_checked(unsigned int b);
+int _putchar(char c);
