2962be2bed7e5c6b0218f1363ef353c68f86de5b
FerchichiXIII <anasferchichi22@gmail.com>
2022-03-03 23:27:48 +0100

    string nconcat

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,5 +2,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <unistd.h>
+#include <string.h>
 void *malloc_checked(unsigned int b);
 int _putchar(char c);
+ char *string_nconcat(char *s1, char *s2, unsigned int n);
