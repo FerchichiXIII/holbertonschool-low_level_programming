cc6fed7ac20e421be647204741d2f3707029ab4c
FerchichiXIII <anasferchichi22@gmail.com>
2022-03-17 16:12:27 +0100

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	int i;
+	if (h == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+			printf("[0](nil)\n");
+		else
+			printf( "[%i] %s\n", h->len, h->str);
+		h = h->next;
+	}
+return (i);
+}
