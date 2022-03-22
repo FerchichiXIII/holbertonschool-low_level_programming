14cab3b6acee40803202eaf56b75ff3f6c05e46a
FerchichiXIII <anasferchichi22@gmail.com>
2022-03-22 14:00:16 +0100

    update liste

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,12 +11,14 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i;
 
-	for (i = 0; h != NULL; i++)
+	i = 0;
+
+	while (h != NULL)
 
 	{
 
-		printf("%i\n", h->n);
 		h = h->next;
+		i++;
 	}
 
 	return (i);
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,12 +10,14 @@ size_t listint_len(const listint_t *h)
 
 {
 	size_t i;
+	i = 0;
 
-	for (i = 0; h != NULL; i++)
+	while ( h != NULL)
 
 	{
 
 		h = h->next;
+		i++;
 	}
 
 	return (i);
