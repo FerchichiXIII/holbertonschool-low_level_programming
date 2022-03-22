c40ccc50fab3a056e653462dc1b16e42d6ae5428
FerchichiXIII <anasferchichi22@gmail.com>
2022-03-22 19:25:27 +0100

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 /**
- * @brief
+ * free_listint2 - frees a listint_t list.
+ * @head: listint_t
  *
- *
- *
+ * Return: nothing.
  */
 
 void free_listint2(listint_t **head)
 
 {
-
 
 	while (head)
 
