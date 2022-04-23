2be5bce8cd95ebf6a8d22f2ff2550b330166f646
FerchichiXIII <anasferchichi22@gmail.com>
2022-04-23 16:35:26 +0100

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * times_table -  prints the 9 times table
+ *
+ *
+ *
+ */
+void times_table(void)
+{
+	int num = 1, res, i, j;
+	for (i = 0; i < 10; i++)
+	{
+
+		for (j = 0; j < 10; j++)
+		{
+			res = num * j;
+			_putchar(res);
+		}
+		num++;
+	}
+	return;
+}
