cee2dbe6d839602eb91626e03e7e59192869f2aa
FerchichiXIII <anasferchichi22@gmail.com>
2022-04-24 23:17:30 +0100

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -19,11 +19,10 @@ void more_numbers(void)
 
 		for (b = 0; b <= 14; b++)
 		{
-			if (b > 10)
+			if (b >= 10)
 				putchar(b / 10 + '0');
 			putchar(b % 10 + '0');
 		}
 		putchar('\n');
 	}
-
 }
