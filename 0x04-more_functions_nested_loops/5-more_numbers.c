19649e5c504fbc030001d88fbe1267fd0a95148b
FerchichiXIII <anasferchichi22@gmail.com>
2022-04-24 23:09:44 +0100

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- * print_most_numbers -  prints the numbers, from 0 to 9,
+ * more_numbers- t prints 10 times the numbers, from 0 to 14
  *
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 void more_numbers(void)
+
 {
 	int a;
 	int b;
