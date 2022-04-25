76fb4dc53f735692fe1e1caeee9f4dee382a4f36
FerchichiXIII <anasferchichi22@gmail.com>
2022-04-25 19:38:05 +0100

    print dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef dog_h
 #define dog_h
+#include <stdio.h>
 /**
  * struct dog - new type struct dog with the following elements
  * @name: char*
@@ -14,5 +15,5 @@ typedef struct dog
 	char *owner;
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
-
