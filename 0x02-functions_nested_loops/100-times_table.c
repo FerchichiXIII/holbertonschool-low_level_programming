b472a7a834c289484bf6ecb7b20cc07de414349f
FerchichiXIII <anasferchichi22@gmail.com>
2022-04-26 14:51:17 +0100

    tme tabla

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_time_table -prints the n times table, starting with 0.
+ * print_time_table - prints the n times table, starting with 0.
  * @n: int
  *
  * Return: 0
