e80d67b34df5b9a3a9cd6717b184a1c7cbc0c62e
FerchichiXIII <anasferchichi22@gmail.com>
2022-07-14 21:02:59 +0100

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """
-	Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
+task 5
 """
 
 
 def island_perimeter(grid):
-    m, n = len(grid), len(grid[0])
-    land, nei = 0, 0
-    for i in range(m):
-        for j in range(n):
+    """
+    Return the perimeter of the island described in grid
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                land += 1
-                if i < m-1 and grid[i+1][j] == 1:
-                    nei += 1
-                if j < n-1 and grid[i][j+1] == 1:
-                    nei += 1
-    return land*4-2*nei
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                if i == len(grid)-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                if j == len(grid[i])-1 or grid[i][j+1] == 0:
+                    perimeter += 1
+    return perimeter
