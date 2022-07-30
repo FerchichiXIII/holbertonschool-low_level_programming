ee34c2fa94cc16d77810dbec896cb56a708031bd
FerchichiXIII <anasferchichi22@gmail.com>
2022-07-30 14:24:05 +0100

    dynamic lib

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# creates a dynamic library called liball.so from all the .c files that are in the current directory.
 
 gcc -Wall -pedantic -Werror -Wextra *.c -c -fPIC
 gcc -Wall -pedantic -Werror -Wextra *.o -shared -o liball.so
